c06021c9c4230dc7d24501332dafe3d1ed329702
Aaron Pana <aaronpana02@gmail.com>
2024-03-07 00:40:05 -0300

    Interactuando con JavaScript

---
+++ Clase-01/registro-estudiantes.js
@@ -0,0 +1,43 @@
+console.log('============================================================================================');
+console.log('>> Infromacion importante:');
+console.log('   Este ejercicio no esta correctamente resuelto para ejecutarse con nodeJS');
+console.log('   La resolucion correcta para nodeJS esta en -> Clase 02/registro-estudiantes.js');
+console.log('!! Gracias por su atencion, que tenga un buen dia :)');
+console.log('============================================================================================');
+
+// const registrarEstudiantes = () => {
+//     console.log('Iniciando registro de estudiante');
+
+//     let cantidadEstudiantes = prompt('Ingresar la cantidad de estudiantes a registrar: ');
+    
+//     let estudiantes = [];
+    
+//     for (let i = 0; i < cantidadEstudiantes; i++) {
+//         let nombreEstudiante = prompt(`Ingrese el nombre del estudiante ${i+1}: `);
+//         let edadEstudiante = +prompt(`Ingrese la edad de ${nombreEstudiante}: `);
+        
+//         console.log('Nombre:', nombreEstudiante, '| Edad:', edadEstudiante);
+        
+//         let estudiante = {
+//             nombre: nombreEstudiante,
+//             edad: edadEstudiante
+//         };
+        
+//         estudiantes.push(estudiante);
+//     }
+    
+//     console.log('Cantidad de estudiantes registrados:', estudiantes.length);
+    
+//     return estudiantes
+// }
+
+// const mostrarListaEstudiantes = (estudiantes) => {
+//     console.log('Lista de estudiantes registrados');
+    
+//     for (let estudiante of estudiantes) {
+//         console.log(`Nombre: ${estudiante.nombre} | Edad: ${estudiante.edad}`);
+//     }
+// }
+
+// let listaEstudiantes = registrarEstudiantes();
+// mostrarListaEstudiantes(listaEstudiantes);
